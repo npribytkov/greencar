34ffb8d14c272ab6d72b60518eebea97cafe0b77
nikita <nikcfc@gmail.com>
2020-12-27 13:47:39 +0300

    add detail page

--- js/main.js
+++ js/main.js
@@ -26,3 +26,55 @@ $(document).on("click", ".mobile_menu_overlay", function(e) {
         $(".mobile_menu_container .activity").removeClass("activity");
     });
 });
+
+/*car detail*/
+$(document).ready(function () {
+    var height = 0,
+        maxHeightBlock = 250;
+
+    $('.detail-card .tabs .tab').each(function (){
+        height = $(this).height();
+        if (height > maxHeightBlock){
+            $(this).addClass('big');
+            $(this).append('<div class="more-block"><a href="#" class="show-more"> > </a></div>')
+        }
+    });
+
+    loadSliderCarDetail();
+})
+/*Change tabs*/
+$(document).on('click', '.detail-card .head a', function (e) {
+    e.preventDefault();
+    var id = $(this).attr('href');
+    if (id) {
+        $('.detail-card .tabs li').removeClass('active');
+        $('.detail-card .tabs .tab').removeClass('active');
+        $(this).parent().addClass('active');
+        $(id).addClass('active');
+    }
+});
+/*Button show more*/
+$(document).on('click', '.show-more', function (e) {
+    e.preventDefault();
+    $(this).parents('.tab').removeClass('big');
+    $(this).parent().remove();
+});
+
+function  loadSliderCarDetail(){
+    var width = $(document).width(),
+        trumbWidth = 260,
+        trumbHeight = 170;
+
+    if (width < 1300){
+        trumbWidth = 130;
+        trumbHeight = 85;
+    }
+
+    $('.detail-card .slider-detail').fotorama({
+        width: '100%',
+        nav: 'thumbs',
+        loop: true,
+        thumbwidth: trumbWidth,
+        thumbheight : trumbHeight,
+    });
+}
